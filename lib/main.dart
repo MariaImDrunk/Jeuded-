b6a2437b2a109645764ea5076460efbdb4bb23c8
MiquelSamuel <samuel.miquel4@gmail.com>
2022-07-27 08:31:39 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DedeApp());
+}
+
+class DedeApp extends StatefulWidget {
+  const DedeApp({Key? key}) : super(key: key);
+
+  @override
+  State<DedeApp> createState() => _DedeAppState();
+}
+
+class _DedeAppState extends State<DedeApp> {
+  int des1 = Random().nextInt(6) + 1;
+  int des2 = Random().nextInt(6) + 1;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('jeudede'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Row(children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: MaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      des1 = Random().nextInt(6) + 1;
+                      des2 = Random().nextInt(6) + 1;
+                    });
+                  },
+                  child: Image.asset("images/dice$des1.png")),
+            ),
+          ),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: MaterialButton(
+                onPressed: () {
+                  setState(() {
+                    des1 = Random().nextInt(6) + 1;
+                    des2 = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset("images/dice$des2.png")),
+          ))
+        ]),
+      ),
+    ));
+  }
+}
